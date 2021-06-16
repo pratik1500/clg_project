0ce487a82cc136f801ddedba1e230136ba6a2a95
pratik1500 <pratik_mahankal@outlook.com>
2021-06-16 16:56:36 +0530

    second commit

--- app.py
+++ app.py
@@ -62,8 +62,6 @@ def predict():
             return render_template("index.html",prediction = disease)
     return render_template("index.html", prediciton = 1)
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 
